4b4ec5bb32ad6e54d830af0085940f683ea3be9f
行比拓也 <gyouhitakuya@MBP-2.local>
2020-11-22 14:59:39 +0900

    記事投稿機能作成

--- index.js
+++ index.js
@@ -2,31 +2,45 @@ const express = require('express')
 const path = require('path')
 const app = new express()
 const mongoose = require('mongoose')
+require('./models/BlogPost')
+const BlogPost = mongoose.model('BlogPost')
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 const ejs = require('ejs')
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true })
 
-app.listen(4000, ()=> {
+app.listen(4000, () => {
   console.log('App listen 4000');
 })
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
   // res.sendFile(path.resolve(__dirname, 'pages/index.html'))
   res.render('index')
 })
-app.get('/about', (req, res)=>{
+app.get('/about', (req, res) => {
   // res.sendFile(path.resolve(__dirname, 'pages/about.html'))
   res.render('about')
 
 })
-app.get('/contact', (req, res)=>{
+app.get('/contact', (req, res) => {
   // res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
   res.render('contact')
 
 })
-app.get('/post', (req, res)=>{
+app.get('/post', (req, res) => {
   // res.sendFile(path.resolve(__dirname, 'pages/post.html'))
   res.render('post')
+})
+app.get('/posts/new', (req, res) => {
+  // res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+  res.render('create')
+})
 
+app.post('/posts/store', async (req, res) => {
+  // model create
+  await BlogPost.create(req.body)
+  res.redirect('/')
 })
