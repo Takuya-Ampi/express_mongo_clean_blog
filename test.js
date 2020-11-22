3ed1a848ae919e8525511cac86c26f32833d4e81
行比拓也 <gyouhitakuya@MBP-2.local>
2020-11-22 13:58:11 +0900

    テストデータの挿入

---
+++ test.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+require('./models/BlogPost.js')
+const BlogPost = mongoose.model('BlogPost')
+
+mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true })
+
+// テストデータの作成(Create)
+// BlogPost.create({
+//   title: 'testTitle',
+//   body: 'WeMakeTestBody'
+// }, (error, blogPost)=> {
+//   console.log(error, blogPost)
+// })
+
+const id = "5fb9ebfcdb73a8fa7f027ee9"
+
+
+// テストデータの検索(Read)
+// BlogPost.find({
+//   title: 'testTitle'
+// }, (error, blogPost)=>{
+//   console.log(error, blogPost)
+// })
+// BlogPost.find({
+//   _id: id
+// }, (error, blogPost)=>{
+//   console.log(error, blogPost)
+// })
+
+// テストデータの更新(Update)
+// BlogPost.findByIdAndUpdate(id,{
+//   title: 'UpdatedTitle',
+// }, (error, blogPost)=>{
+//   console.log(error, blogPost)
+// })
+
+// テストデータの削除(Delete)
+// BlogPost.findByIdAndDelete(id, (error, blogPost)=>{
+//   console.log(error, blogPost)
+// })
